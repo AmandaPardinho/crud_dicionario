7cd183ce84a5e18d432d8dcf9831e6c500e7a5c8
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2023-04-19 20:55:40 -0300

    Alterações nos arquivos do fork do repositório da aula

--- brincando_com_repositorio.py
+++ brincando_com_repositorio.py
@@ -1,10 +1,13 @@
 import repositorio
 
 #EXPERIMENTE A FUNÇÃO retornar_produtos(), que retornar um dicionário de dicionários com todos os produtos
+print(repositorio.retornar_produtos())
 
 #EXPERIMENTE A FUNÇÃO retornar_produto(id), que recebe um id e retorna um único dicionário contendo os detalhes do produto
+#print(repositorio.retornar_produto(1))
 
 #EXPERIMENTE A FUNÇÃO remover_produto(id), que recebe um id e remove um produto do dicionário de dicionários
+print(repositorio.remover_produto(1))
 
 #EXPERIMENTE A FUNÇÃO alterar_produto(id, dados_produto), que recebe um id e um dicionário contendo dados do produto para então atualizar o produto no dicionário de dicionários
 
